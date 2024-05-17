ebc7e91b494f7f10e2b29bb33c4009c8f0b9ea81
unityyang <766684931@qq.com>
2024-05-17 14:38:02 +0800

    config build environment

--- make_android.sh
+++ make_android.sh
@@ -11,7 +11,5 @@ export ANDROID_NDK_ROOT_PATH=%ANDROID_NDK%
 mkdir -p build-android-v8a
 cd build-android-v8a
 cmake -DBUILD_ANDROID=On -DANDROID_ABI=arm64-v8a \
-	# -DUSE_INTERCEPTOR_LIB=On -DLLVM_DIR=/E/llvm_4.0_src/llvm/build_native/install_arm64/lib/cmake/llvm \
-	# -DBUILD_VERSION_HASH=cc05b288b6d1660ab04c6cf01173f1bb62e6f5dd \
 	-G "MinGW Makefiles" ..
 /d/Sdk/ndk/android-ndk-r21e/prebuilt/windows-x86_64/bin/make.exe VERBOSE=1
